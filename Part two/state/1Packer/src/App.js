bbc7dc05040546ab1ffca09085ad3f8549283aeb
beka-tech <abeka039@gmail.com>
2024-01-02 15:22:26 +0300

    React Project

--- Part two/state/1Packer/src/App.js
+++ Part two/state/1Packer/src/App.js
@@ -1,5 +1,7 @@
 // import React, { useState } from "react";
 
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -25,21 +27,50 @@ function Logo() {
   return <h1>Far From Home</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("");
   function handleSubmit(e) {
-    e.PreventDefault();
+    e.preventDefault();
     console.log(e);
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: `${new Date().getMinutes().toString().padStart(2, "0")}${new Date()
+        .getSeconds()
+        .toString()
+        .padStart(2, "0")}`,
+    };
+    console.log(newItem);
+    setDescription("");
+    setQuantity("");
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your tips</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setQuantity(e.target.value);
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item..."></input>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      ></input>
       <button>Add</button>
     </form>
   );
@@ -50,7 +81,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -58,15 +89,20 @@ function PackingList() {
 }
 
 function Item({ item }) {
+  const [isopen, setIsopen] = useState(true);
   return (
     <>
-      <li>
-        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-          {item.quantity}
-          {item.description}
-        </span>
-        <button> ❌ &times;</button>
-      </li>
+      {isopen ? (
+        <li>
+          <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+            {item.quantity}
+            {item.description}
+          </span>
+          <button onClick={() => setIsopen((Is) => !Is)}> ❌ &times;</button>
+        </li>
+      ) : (
+        ""
+      )}
     </>
   );
 }
