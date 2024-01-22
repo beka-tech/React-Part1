398a62cd7d674a89b28bbd2bb272b3e605acde2d
beka aman <abeka039@gmail.com>
2024-01-22 10:54:45 +0300

    new react project with tailwind
    new react project with tailwind

---
+++ Tailwind/my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <p className="text-6xl">Hellolo</p>;
+}
+
+export default App;
---
+++ Tailwind/tailwind-react/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-6xl ">Heel</p>
+    </div>
+  );
+}
+
+export default App;
