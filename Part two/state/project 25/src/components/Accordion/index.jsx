b36a890ad33c8dfe9b9c2e9f5f57d8da47617352
beka aman <abeka039@gmail.com>
2024-02-23 09:20:42 +0300

    Accordion
    Accordion

--- Part two/state/project 25/src/components/Accordion/index.jsx
+++ Part two/state/project 25/src/components/Accordion/index.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
 export default function Accordian() {
-  return <div>Accoridian 2</div>;
+  const [isSelected, setIsSelected] = useState(null);
+
+  function passid(getcurid) {
+    setIsSelected(getcurid === isSelected ? null : getcurid);
+  }
+
+  console.log(isSelected);
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data.map((dataItem) => (
+          <div className="item" key={dataItem.id}>
+            <div onClick={() => passid(dataItem.id)} className="title">
+              <h2>{dataItem.question}</h2>
+              <span>+</span>
+              {isSelected === dataItem.id ? (
+                <div>
+                  <h2 className="clicked">{dataItem.answer}</h2>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
